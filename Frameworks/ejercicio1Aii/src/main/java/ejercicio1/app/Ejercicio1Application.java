d644874c99b1c22e3091dabb8b4bd2cebd2d39de
zoezapettini <141296728+zoezapettini@users.noreply.github.com>
2024-06-02 14:58:38 -0300

    no me escribe nada en el texto, preguntar

---
+++ Frameworks/ejercicio1Aii/src/main/java/ejercicio1/app/Ejercicio1Application.java
@@ -0,0 +1,31 @@
+package ejercicio1.app;
+
+import java.io.IOException;
+
+import javax.swing.SwingUtilities;
+
+import ejercicio1.ui.WallPostUI;
+
+import java.util.logging.*;
+
+public class Ejercicio1Application {
+
+	
+	
+	public static void main(String[] args) throws SecurityException, IOException {
+		Logger modeloLogger= Logger.getLogger("modelo");
+		Logger interfazLogger= Logger.getLogger("interfaz");
+		modeloLogger.setLevel(Level.WARNING);
+		interfazLogger.setLevel(Level.INFO);
+        FileHandler fileHandler = new FileHandler("registro.txt");
+        fileHandler.setFormatter(new SimpleFormatter());
+        modeloLogger.addHandler(fileHandler);
+        SwingUtilities.invokeLater(new Runnable() {
+        	public void run() {
+        		new WallPostUI();
+        	}
+        });
+	
+
+}
+}
